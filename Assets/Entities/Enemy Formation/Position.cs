a62d6fd4e97d35e508048840296fc6c727e4ede4
Anupam <anusrivastava910@gmail.com>
2020-11-26 12:37:13 +0530

    Add files

---
+++ Assets/Entities/Enemy Formation/Position.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Position : MonoBehaviour {
+
+	void OnDrawGizmos() {
+		// Gizmo representing enemy positions within the editor
+		Gizmos.DrawWireSphere(transform.position, 1);
+	}
+}
